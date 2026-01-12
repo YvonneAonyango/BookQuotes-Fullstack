0f8e18af5db901db9f55d3a3e5f5db188115aaac
YvonneAonyango <166454264+YvonneAonyango@users.noreply.github.com>
2026-01-13 00:45:12 +0100

    debugg interceptor file

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,8 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+
+  // Skip static assets (translations, images, etc.)
+  if (req.url.includes('/assets/')) {
+    return next(req);
+  }
+
   console.log('Interceptor triggered for:', req.url);
-  
+
   const token = localStorage.getItem('authToken');
   console.log('Token exists:', !!token);
   console.log('Token value:', token ? `${token.substring(0, 20)}...` : 'none');
@@ -13,10 +19,10 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${token}`
       }
     });
-    
+
     console.log('Adding Authorization header');
     console.log('Final headers:', clonedReq.headers.keys());
-    
+
     return next(clonedReq);
   }
 
